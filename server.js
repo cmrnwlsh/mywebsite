965e3acda0de7770990a5b9538d43036e81bd1c6
Cameron Walsh <cmrnwlsh@gmail.com>
2022-10-24 23:18:23 -0400

    added redirect from http to https

--- server.js
+++ server.js
@@ -8,15 +8,21 @@ const favicon = require('serve-favicon')
 const server = express();
 
 server.use(helmet());
+server.use((req, res, next) => {
+    if (req.headers['x-forwarded-proto'] !== 'https')
+        return res.redirect('https://' + req.headers.host + req.url);
+    else
+        return next();
+});
 server.use(logger(':date[iso] :remote-addr'));
 server.use(logger('dev'));
 server.use(express.json());
-server.use(express.urlencoded({ extended: false }));
+server.use(express.urlencoded({extended: false}));
 server.use(cookieParser());
 server.use("/static", express.static(path.join(__dirname, "client", "build", "static")))
 server.use(favicon(path.join(__dirname, 'client', 'public', 'favicon.ico')))
 server.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
 });
 
 module.exports = server;
