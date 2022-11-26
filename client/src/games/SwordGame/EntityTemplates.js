33adc8abedb6b30f3398acf98f467709cab4b4d5
Cameron Walsh <cmrnwlsh@gmail.com>
2022-11-25 20:22:03 -0500

    There is now a sword that you can pick up and put into your inventory with spacebar

--- client/src/games/SwordGame/EntityTemplates.js
+++ client/src/games/SwordGame/EntityTemplates.js
@@ -1,15 +1,33 @@
+
 export class Player {
     constructor(x, y) {
         this.x = x;
         this.y = y;
+        this.inventory = [];
         this.symbol = '@'
     }
 }
 
 export class Floor {
-    constructor(x, y) {
+    constructor(x, y, contents = []) {
         this.x = x;
         this.y = y;
-        this.symbol = '.'
+        this.contents = contents;
+        this.symbol = this.contents.length ? this.contents.slice(-1)[0].symbol : '.';
+    }
+}
+class Weapon {
+    constructor() {
+        this.damage = {bronze: 1, iron: 2, steel: 3};
+    }
+}
+
+export class Sword extends Weapon {
+    constructor(material) {
+        super();
+        this.type = 'sword';
+        this.material = material;
+        this.damage = this.damage[material] + 3;
+        this.symbol = '/';
     }
 }
--- client/src/games/SwordGame/SwordGame.js
+++ client/src/games/SwordGame/SwordGame.js
@@ -4,40 +4,61 @@ import {Col, Row, Tab, Tabs} from "react-bootstrap";
 import './SwordGame.css'
 import {useState} from "react";
 import {Floor, Player} from "./EntityTemplates";
+import './EntityMaps'
+import {maps} from "./EntityMaps";
 
 export default function SwordGame() {
-    const range = Array.from(Array(24));
     const [[px, py], setPlayerPos] = useState([12, 12]);
-    const [ents, setEnts] = useState(range.map((row, i) => range.map((col, j) =>
-        i === py && j === px ? new Player(i, j) : new Floor(i, j))));
+    const [ents, update] = useState(maps.baseMap(px, py));
 
-    console.log(px + " " + py);
+    function movePlayer(coords) {
+        const [x, y] = coords
 
-    function move(ent, coords) {
-        ents[ent.y][ent.x] = new Floor(ent.x, ent.y);
-        ent.x = coords[0];
-        ent.y = coords[1];
-        ents[ent.y][ent.x] = ent;
-
-        if (ent instanceof Player)
+        if (ents[y][x] instanceof Floor) {
+            ents[y][x] = new Floor(x, y, ents[y][x].contents.concat(ents[py][px].contents.slice(-1)[0]));
+            ents[py][px] = new Floor(px, py, ents[py][px].contents.slice(0, ents[py][px].contents.length - 1));
             setPlayerPos(coords);
+            update(ents);
+        }
+    }
 
-        setEnts(ents);
+    function pickupItem(index) {
+        let items;
+        let contents = [];
+
+        if(ents[py][px].contents.length > 1) {
+            items = ents[py][px].contents.slice(0, ents[py][px].contents.length - 1);
+            ents[py][px].contents.slice(-1)[0].inventory =
+                ents[py][px].contents.slice(-1)[0].inventory.concat(items[index]);
+
+            items.forEach((element, i) => {
+                if(i !== index)
+                    contents.push(element);
+            })
+
+            ents[py][px] = new Floor(px, py, contents.concat(ents[py][px].contents.slice(-1)[0]))
+            console.log(ents[py][px].contents);
+
+            update([...ents]);
+        }
     }
 
     function keyHandler(event) {
         console.log(event.key);
         if (event.key === 'ArrowUp')
-            move(ents[py][px], [px, (py - 1) >= 0 ? (py - 1) : range.length - 1]);
+            movePlayer([px, (py - 1) >= 0 ? (py - 1) : ents.length - 1]);
 
         if (event.key === 'ArrowDown')
-            move(ents[py][px], [px, Math.abs(py + 1) % range.length]);
+            movePlayer([px, (py + 1) % ents.length]);
 
         if (event.key === 'ArrowLeft')
-            move(ents[py][px], [(px - 1) >= 0 ? (px - 1) : range.length - 1, py]);
+            movePlayer([(px - 1) >= 0 ? (px - 1) : ents[py].length - 1, py]);
 
         if (event.key === 'ArrowRight')
-            move(ents[py][px], [Math.abs(px + 1) % range.length, py]);
+            movePlayer([(px + 1) % ents[py].length, py]);
+
+        if (event.keyCode === 32)
+            pickupItem(0);
     }
 
     return (
@@ -46,13 +67,19 @@ export default function SwordGame() {
             <Container className={'GameTabs'}>
                 <Tabs fill>
                     <Tab eventKey={'game'} title={'Game'}>
-                        <Container className={"SwordGame"} onKeyDown={keyHandler} tabIndex={0}>
-                            {ents.map(row => <Row>{row.map(ent =>
-                                <Col className={"Tile"}>{ent.symbol}</Col>)}</Row>)}
+                        <Container className={"SwordGame border"} onKeyDown={keyHandler} tabIndex={0}>
+                            {ents.map((row, i) =>
+                                <Row key={i + ':'}>{row.map((ent, j) =>
+                                <Col key={i + ':' + j} className={"Tile"}>{ent.symbol}</Col>)}</Row>)}
                         </Container>
                     </Tab>
                     <Tab eventKey={'inventory'} title={'Inventory'}>
-                        <Container className={'Inventory'}></Container>
+                        <Container className={'Inventory'}>
+                            {ents[py][px].contents.slice(-1)[0].inventory.map((row, i) =>
+                                <Row key={i * 30}>
+                                    {Object.keys(row).map(key => ' | ' + key + ': ' + row[key])}
+                                </Row>)}
+                        </Container>
                     </Tab>
                 </Tabs>
             </Container>
